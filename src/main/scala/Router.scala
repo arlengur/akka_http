9f4b39002a98d308ce5fb1ec3f7fc9dc2bf967f0
Miguel Lopez <miguel.lopez@disneystreaming.com>
2019-02-01 10:44:50 +0000

    Fixes

--- src/main/scala/Router.scala
+++ src/main/scala/Router.scala
@@ -30,21 +30,6 @@ class TodoRouter(todoRepository: TodoRepository) extends Router with Directives 
             }
           }
         }
-      } ~ path(Segment) { id: String =>
-        put {
-          entity(as[UpdateTodo]) { updateTodo =>
-            validateWith(UpdateTodoValidator)(updateTodo) {
-              handle(todoRepository.update(id, updateTodo)) {
-                case TodoRepository.TodoNotFound(_) =>
-                  ApiError.todoNotFound(id)
-                case _ =>
-                  ApiError.generic
-              } { todo =>
-                complete(todo)
-              }
-            }
-          }
-        }
       }
       // ~ - chains two routes together, if the first one rejects the request, it gives the second route a chance to match it
       // path - matches the remaining path after consuming a leading slash
@@ -55,6 +40,32 @@ class TodoRouter(todoRepository: TodoRepository) extends Router with Directives 
     } ~ path("pending") {
       handleWithGeneric(todoRepository.pending()) {
         todos => complete(todos)
+      }
+    } ~ path(Segment) { id: String =>
+      /*
+       This route has to be outside the directive `pathEndOrSingleSlash`,
+       because it only matches paths that are matched completely, so if the request has anything
+       after the last / it won't match, for example, it will match `/todos/` or `/todos`,
+       but it won't match `/todos/1`.
+
+       For reference, it's documentation says:
+
+          Only passes on the request to its inner route if the request path has been matched
+          completely or only consists of exactly one remaining slash.
+        */
+      put {
+        entity(as[UpdateTodo]) { updateTodo =>
+          validateWith(UpdateTodoValidator)(updateTodo) {
+            handle(todoRepository.update(id, updateTodo)) {
+              case TodoRepository.TodoNotFound(_) =>
+                ApiError.todoNotFound(id)
+              case _ =>
+                ApiError.generic
+            } { todo =>
+              complete(todo)
+            }
+          }
+        }
       }
     }
   }
